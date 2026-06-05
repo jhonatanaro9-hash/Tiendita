44a906bc109ac1925ed7af190e849b01b606e448
IINDRA <jhonatanaro9@gmail.com>
2026-06-05 15:17:20 +0000

    proyecto tienda

---
+++ tiendia.py
@@ -0,0 +1,8 @@
+x = float(5)
+contador = 0
+while x>=1.5 
+print ("su saldo es de ". x, "Bs.")
+print("Puedes comprar:")
+print("1. Papas (Bs. 1.50) ")
+print("2. Chocolate (Bs. 2.00)")
+print("3. Reffresco (Bs. 2.50)")
